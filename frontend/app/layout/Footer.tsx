3c23c566c036637a2c807557a9f85916d9e16fa3
demi-repo <cdadev24@gmail.com>
2024-11-07 02:09:35 -0500

    fix: update the footer mobile responsive

--- frontend/app/layout/Footer.tsx
+++ frontend/app/layout/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t border-gray-500 max-w-[1120px] text-sm px-[60px]">
-      <div className="flex flex-wrap justify-center md:justify-between flex-row px-6 py-5 text-base gap-4">
-        <div className="flex flex-row gap-4">
+    <footer className="w-full border-t border-gray-500 max-w-[1120px] text-sm px-4 sm:px-[60px]">
+      <div className="flex flex-wrap justify-center md:justify-between flex-row px-6 py-5 text-base gap-2 sm:gap-4">
+        <div className="flex flex-row gap-2 sm:gap-4 flex-wrap justify-center">
           <Link href="/privacy-policy" className="hover:text-blue-500 transition-all">
             Privacy Policy
           </Link >
@@ -12,9 +12,9 @@ const Footer = () => {
             Terms of Service
           </Link>
         </div>
-        <span>
-          &copy; 2024 Vigilant Owl · All rights reserved.
-        </span>
+        <div className="flex flex-wrap justify-center flex-row text-sm sm:text-base">
+          <div>&copy; 2024 Vigilant Owl</div><div>·All rights reserved.</div>
+        </div>
       </div>
     </footer>
   )
