273a0ce3254dcce5443882c1555ab17d91c19e51
demi-repo <cdadev24@gmail.com>
2024-11-12 18:42:47 -0500

    fix: error in report controller

--- backend-mvp/src/controllers/report.controller.js
+++ backend-mvp/src/controllers/report.controller.js
@@ -1,19 +1,27 @@
 module.exports = {
   getReport: async (req, res) => {
-    const { groupId, phoneNumber, startDate, endDate, tableId } = req.body;
-    const report = await global.ai.generateReport(
-      groupId,
-      phoneNumber,
-      startDate,
-      endDate,
-      tableId
-    );
+    try {
+      const { groupId, phoneNumber, startDate, endDate, tableId } = req.body;
+      const report = await global.ai.generateReport(
+        groupId,
+        phoneNumber || "18406880000",
+        startDate,
+        endDate,
+        tableId || "1"
+      );
 
-    console.log(report);
+      console.log(report);
 
-    return res.status(200).json({
-      status: "success",
-      data: report,
-    });
+      return res.status(200).json({
+        status: "success",
+        data: report,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(400).json({
+        status: "error",
+        message: err.message,
+      });
+    }
   },
 };
